c101a16fe45b2603bac1013606cfcf286411eb11
Telmo Pires <telmo.pires@gmail.com>
2013-02-10 19:26:23 +0000

    getting filename by 'path' module

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 var http = require('http');
 var fs = require('fs');
- 
+var path = require('path');
+
 function getmyfileplz(url) {
- 
-  var filename = url.substr(url.lastIndexOf('/') + 1, url.length);
-  var file = fs.createWriteStream(filename);
- 
- 
+  
+  var file = fs.createWriteStream(path.basename(url));
+
   http.get(url, function(res) {
- 
+
     var fileBytes = res.headers['content-length'];
     var downloadedBytes = 0;
- 
+
     console.log("status code received: " + res.statusCode);
- 
+
     res.pipe(file);
- 
-    res.once('data', function() {
+
+    res.once('data', function(chunk) {
+      console.log(chunk.length);
       console.log('downloading...');
     });
- 
+
     res.on('data', function(chunk) {
       downloadedBytes += chunk.length;
-      var progress = (downloadedBytes / fileBytes) * 100;
-      console.log("progress: " + parseInt(progress, 10) + "%");
+      var progress = parseInt((downloadedBytes / fileBytes) * 100, 10);
+      console.log("progress: " + progress + "%");
     });
- 
+
     res.on('end', function() {
       console.log(file.path + " downloaded!");
     });
- 
+
   });
 }
- 
+
 getmyfileplz('http://twitter.github.com/bootstrap/assets/bootstrap.zip');
